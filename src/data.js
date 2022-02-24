9e35da946ad9b88995e980a196d567777662e46c
KhongBinhMinh <minhkbph17672@fpt.edu.vn>
2022-02-24 13:10:32 +0700

    data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,10 @@
 const data = [
-    {
 
+    {
+        title: "Điện thoại iPhone 13 mini 128GB",
+        img: "http://res.cloudinary.com/djultth5g/image/upload/v1645678630/utv7g6jeiqhz9gzlbfcp.jpg",
+        desc: "Tất cả dòng iPhone 12 đều được hỗ trợ kết nối 5G ",
+        id: 67,
     },
     {
 
